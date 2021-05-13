5ba379db10f831e43164a03dd877c84cb3311916
Patobeur <patobeur41@gmail.com>
2021-05-13 14:53:03 +0200

    re maj

--- private/index.php
+++ private/index.php
@@ -1,5 +1,5 @@
 <?php
 	session_start();
 	session_destroy();
-	header("Location: ".'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']));
+	header("Location: http".(empty($_SERVER['HTTPS']) ? '': 's').'://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']));
 ?>
